e1a77435e688934583e137258732945576fbfaa8
MadukaMalshan <malshanmaduka8@gmail.com>
2026-01-13 18:32:36 +0530

    Setup routings

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link"
+
 export default function page() {
   return (
     <main>
       <h1>DripCulture</h1>
       <p>Street Clothing store</p>
+
+      <Link href="/shop">
+        <button>Shop Now</button>
+      </Link>
     </main>
   )
 }
