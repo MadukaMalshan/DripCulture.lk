affabe96d3db20d8c6d8c2bc0a713a2fadcfcef8
MadukaMalshan <malshanmaduka8@gmail.com>
2026-01-13 18:28:14 +0530

    Add dummy product list

---
+++ app/data/product.ts
@@ -0,0 +1,23 @@
+export const products = [
+  {
+    id: "dc-001",
+    name: "Oversized Black Tee",
+    price: 3500,
+    image: "/placeholder.png",
+    description: "Premium oversized streetwear t-shirt",
+  },
+  {
+    id: "dc-002",
+    name: "Oversized White Tee",
+    price: 3200,
+    image: "/placeholder.png",
+    description: "Clean white drip for daily wear",
+  },
+  {
+    id: "dc-003",
+    name: "Purple Flame Tee",
+    price: 3800,
+    image: "/placeholder.png",
+    description: "Signature DripCulture flame design",
+  },
+];
