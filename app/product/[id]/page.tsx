3e26ea51de7014600f4b839e1e3c095f55ecdda9
MadukaMalshan <malshanmaduka8@gmail.com>
2026-01-13 18:41:22 +0530

    Create product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,13 +1,27 @@
+import { products } from "../../data/product";
+
 interface ProductPageProps {
     params: Promise<{ id: string }>;
 }
 
 export default async function ProductPage({ params }: ProductPageProps) {
     const { id } = await params;
+
+    const product = products.find((product) => product.id === id);
+
+    if (!product) {
+        return (
+
+            <h1>Product not found</h1>
+        );
+    }
     return (
         <main>
-            <h1>Product ID: {id}</h1>
-            <p>Product details will load here</p>
+            <h1>{product.name}</h1>
+            <p>{product.description}</p>
+            <p>Price: Rs.{product.price}</p>
+            
+            <button>Add to Cart</button>
         </main>
     );
 }
