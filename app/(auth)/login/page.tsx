3d8ab86683aee9927aec2becbcdbfedf697ce4e7
MadukaMalshan <malshanmaduka8@gmail.com>
2026-01-13 18:44:47 +0530

    Create login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,15 @@
+export default function LoginPage() {
+  return (
+    <main>
+        <h1>Login</h1>
+
+        <form>
+            <input type="email" placeholder="Email"/>
+            <br />
+            <input type="password" placeholder="Password" />
+            <br />
+            <button type="submit">Login</button>
+        </form>
+    </main>
+  )
+}
