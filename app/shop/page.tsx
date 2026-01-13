177adfa0c612e2f8378a774b9eaa1df4e9d8fb78
MadukaMalshan <malshanmaduka8@gmail.com>
2026-01-13 18:37:23 +0530

    Create shop page

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,8 +1,23 @@
-export default function page() {
+import Link from "next/link"
+import { products } from "../data/product"
+
+export default function ShopPage() {
   return (
     <main>
         <h1>Shop</h1>
-        <p>All products will show here</p>
+        
+        <ul>
+          {products.map((product)=>(
+            <li key={product.id}>
+              <h3>{product.name}</h3>
+              <p>Rs.{product.price}</p>
+
+              <Link href={``}>
+              View Products
+              </Link>
+            </li>
+          ))}
+        </ul>
     </main>
   )
 }
